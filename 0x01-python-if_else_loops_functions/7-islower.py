6b6f0a345a9b4e640336c3e28a398fe18d96a2e4
streetprophet90 <hamoafo@gmail.com>
2023-03-09 06:12:23 -0800

    if_else_loops

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,9 @@
+i#!/usr/bin/python3
+# Author - Harry Fynn
+
+def islower(c):
+    """Function checks for lowercase characters."""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
